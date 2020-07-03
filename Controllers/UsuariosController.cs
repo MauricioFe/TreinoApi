4d371bae54d3ba1e2931f8cb94fe34a77d3b271c
MauricioFe <mauricio.lacerdaml@gmail.com>
2020-07-03 14:27:03 -0300

    Acessando o banco de dados para retornar todos os usuários

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TreinoApi.Models;
+
+namespace TreinoApi.Controllers
+{
+    public class UsuariosController : ApiController
+    {
+        private static List<Usuario> usuarios = new List<Usuario>();
+
+        //[HttpGet] /*Quando eu crio o método com o nome do verbo rest eu não preciso indicar qual verbo vai ser utilizado*/
+        public List<Usuario> Get()
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Treinamento\Android\3º Etapa\Api com XML\Api\TreinoApi\TreinoApi\App_Data\ApiXml.mdf;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("Selec * from Usuario", conn);
+            return usuarios;
+        }
+
+        public void Post(int id, string nome, int idade)
+        {
+            if (!string.IsNullOrEmpty(nome) && id != 0 && idade != 0)
+                usuarios.Add(new Usuario(id, nome, idade));
+        }
+    }
+}
---
+++ Models/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TreinoApi.Models
+{
+    public class Usuario
+    {
+        public int Id{ get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public Usuario(int id, string nome, int idade)
+        {
+            this.Id = id;
+            this.Nome = nome;
+            this.Idade = idade;
+        }
+    }
+}
